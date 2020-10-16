f91a7e703e7aec7d84e2d5b4ea5f6b7ddf6146c2
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-10-16 18:38:17 -0400

    Add files via upload
    Modified to show dashboard and nav

--- FrontEnd/quick-pick/src/App.js
+++ FrontEnd/quick-pick/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-
+import HeaderComponent from './component/HeaderComponent'
+import FooterComponent from './component/FooterComponent'
+import ThirdComponent from './component/ThirdComponent'
+import Counter from './component/Counter/Counter'
+import Login from './component/Login'
+import logo from './logo.svg';
+import Navigation from './component/Navigation'
+import Dashboard from './component/Dashboard'
 import './App.css';
-
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-import QuickPickApplication from './component/quickPick/QuickPickApplication';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <QuickPickApplication/>
+          <Dashboard/>
+          <Navigation index={"dash"}/>
       </div>
     );
   }
