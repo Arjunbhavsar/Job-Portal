2ecff255adf2800bfa60266d687931c66fefa8b2
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-10-16 22:27:13 -0400

    Add files via upload

--- FrontEnd/quick-pick/src/component/quickPick/Navigation.jsx
+++ FrontEnd/quick-pick/src/component/quickPick/Navigation.jsx
@@ -1,41 +1,128 @@
 import React from 'react';
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Navigation.css'
-import logo from '../quickpick-logo2-transparent-small.png'
+import logo from './quickpick-logo2-transparent-small.png'
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from "@material-ui/core/IconButton";
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { Navigation } from '@material-ui/icons';
 
-class Navgiation extends Component {
-
-    constructor(props) {
-        super(props)
-        this.index = this.props.index;
-        this.status = this.props.status;
-        this.dash = "hidden";
-        this.profile = "hidden";
-        this.other = "hidden";
-        if (this.index == "dash"){
-            this.dash = "active";
-        } else if (this.index == "profile") {
-            this.profile = "active";
-        } else if (this.index == "other") {
-            this.other = "active";
-        }
+const useStyles = makeStyles((theme) => ({
+    grow: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block"
+      }
+    },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(3),
+        width: "auto"
+      }
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    inputRoot: {
+      color: "inherit"
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch"
+      }
+    },
+    sectionDesktop: {
+      display: "none",
+      [theme.breakpoints.up("md")]: {
+        display: "flex"
+      }
     }
+  }));
 
-    render(){
-        return (
-            <div class="navBar" id={this.index}>
-                <img src={logo} alt="logo" class="logo"/>
-                <div class="navControls">
-                    <table>
-                        <td><NavButton page={this.dash} name={"Dash"} /></td>
-                        <td><NavButton page={this.profile} name={"Profile"} /></td>
-                        <td><NavButton page={this.other} name={"Other"} /></td>
-                    </table>
-                </div>
-                <SmallLogin status={this.status}/>
+export default function Navgiation() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const isMenuOpen = Boolean(anchorEl);
+    const menuId = "primary-search-account-menu";
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const renderMenu = (
+        // Add information here to check if already logged in and have a seperate menu with if conditionals
+        <Menu
+          anchorEl={anchorEl}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          class="thing"
+        //   id={menuId}
+          keepMounted
+          transformOrigin={{ vertical: "top", horizontal: "right" }}
+          open={isMenuOpen}
+          onClose={handleMenuClose}
+        >
+          <MenuItem onClick={handleMenuClose} id="accountIconMenuItem"><Link to="/login" class="profileMenu">Login</Link></MenuItem>
+        </Menu>
+      );
+    //   id={this.index}
+    return (
+        <div class="navBar"> 
+            <img src={logo} alt="logo" class="logo"/>
+            <div class="navControls">
+                
+                <table>
+                    <td><NavButton page={"hidden"} name={"Dash"} to={"/"}/></td>
+                    <td><NavButton page={"hidden"} name={"Profile"} to={"/profile"}/></td>
+                    <td><NavButton page={"hidden"} name={"Other"} /></td>
+                </table>
             </div>
-        )
-    }
+            <MenuItem onClick={handleProfileMenuOpen} class="account">
+                <IconButton >
+                    <AccountCircle id="accountIcon"/>
+                </IconButton>
+            </MenuItem>
+            {renderMenu}
+        </div>
+    )
+    
+}
+
+Navigation.defaultProps = {
+    index: "dash"
 }
 
 class NavButton extends Component {
@@ -43,8 +130,9 @@ class NavButton extends Component {
         super();
     }
     render(){
+        
         return(
-            <a class="navButton" id={this.props.page} href="">{this.props.name}</a>
+            <Link class="navButton" id={this.props.page} to={this.props.to}>{this.props.name}</Link>
         )
     }
 }
@@ -54,24 +142,11 @@ class SmallLogin extends Component {
         super();
     }
     render(){
-        if (this.props.status == "Login"){
-            return(
-                <div class="smallLog">
-                    <a class="loginButton" href="">{this.props.status}</a>
-                </div>
-            )
-        } else {
-            return(
-                <div class="smallLog">
-                    <p class="loginButton" href="">{this.props.status}</p>
-                </div>
-            )
-        }
+        return(
+            <div class="smallLog">
+                <Link><AccountCircle class="account"/></Link>
+            </div>
+        )
     }
 }
 
-SmallLogin.defaultProps = {
-    status: "Login"
-}
-
-export default Navgiation
---
+++ FrontEnd/quick-pick/src/component/quickPick/Routing.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Navgiation from './Navigation';
+// import AuthenticatedRoute from './AuthenticatedRoute';
+// import RegisterComponent from './RegisterComponent';
+// import WelcomeComponent from './WelcomeComponent';
+// import LoginComponent from './LoginComponent';
+// import Profile from './Profile';
+import ErrorComponent from './ErrorComponent';
+// import FooterComponent from './FooterComponent';
+
+class AppRouting extends Component {
+    render() {
+      return (
+        <div className="applicationHome">
+            <Router>
+                <>
+                    <Navgiation/>
+                    <Switch>
+                        <Route path="/" exact component= {Dashboard}/>
+                        {/* <Route path="/profile" component= {Profile}/> */}
+                        {/* <Route path="/login" component= {LoginComponent}/> */}
+                        {/* <Route path="/register" component= {RegisterComponent}/> */}
+                        {/* <AuthenticatedRoute path="/dashboard/:name" component= {WelcomeComponent}/> */}
+                        <Route component = {ErrorComponent}/>
+                    </Switch>
+                    {/* <FooterComponent/> */}
+                </>
+            </Router>
+        </div>
+      );
+    }
+  }
+
+
+export default AppRouting
