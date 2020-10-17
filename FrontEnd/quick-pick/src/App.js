1b9a0aac3b1d3e8cb33b629a3ff69605000dae84
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-10-16 22:28:14 -0400

    Add files via upload

--- FrontEnd/quick-pick/src/App.js
+++ FrontEnd/quick-pick/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import HeaderComponent from './component/HeaderComponent'
-import FooterComponent from './component/FooterComponent'
 import ThirdComponent from './component/ThirdComponent'
-import Counter from './component/Counter/Counter'
-import Login from './component/Login'
 import logo from './logo.svg';
-import Navigation from './component/Navigation'
-import Dashboard from './component/Dashboard'
+import AppRouting from './component/quickPick/Routing'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Dashboard/>
-          <Navigation index={"dash"}/>
+          <AppRouting/>
       </div>
     );
   }
