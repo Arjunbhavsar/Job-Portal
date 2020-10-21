e30ad5b4e1c58601197fb507e846cd15d4139b39
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-10-21 14:35:03 -0400

    Add files via upload

--- FrontEnd/quick-pick/src/component/quickPick/Navigation.jsx
+++ FrontEnd/quick-pick/src/component/quickPick/Navigation.jsx
@@ -25,43 +25,43 @@ const useStyles = makeStyles((theme) => ({
         display: "block"
       }
     },
-    search: {
-      position: "relative",
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: fade(theme.palette.common.white, 0.25)
-      },
-      marginRight: theme.spacing(2),
-      marginLeft: 0,
-      width: "100%",
-      [theme.breakpoints.up("sm")]: {
-        marginLeft: theme.spacing(3),
-        width: "auto"
-      }
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
-    },
-    inputRoot: {
-      color: "inherit"
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: {
-        width: "20ch"
-      }
-    },
+    // search: {
+    //   position: "relative",
+    //   borderRadius: theme.shape.borderRadius,
+    //   backgroundColor: fade(theme.palette.common.white, 0.15),
+    //   "&:hover": {
+    //     backgroundColor: fade(theme.palette.common.white, 0.25)
+    //   },
+    //   marginRight: theme.spacing(2),
+    //   marginLeft: 0,
+    //   width: "100%",
+    //   [theme.breakpoints.up("sm")]: {
+    //     marginLeft: theme.spacing(3),
+    //     width: "auto"
+    //   }
+    // },
+    // searchIcon: {
+    //   padding: theme.spacing(0, 2),
+    //   height: "100%",
+    //   position: "absolute",
+    //   pointerEvents: "none",
+    //   display: "flex",
+    //   alignItems: "center",
+    //   justifyContent: "center"
+    // },
+    // inputRoot: {
+    //   color: "inherit"
+    // },
+    // inputInput: {
+    //   padding: theme.spacing(1, 1, 1, 0),
+    //   // vertical padding + font size from searchIcon
+    //   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    //   transition: theme.transitions.create("width"),
+    //   width: "100%",
+    //   [theme.breakpoints.up("md")]: {
+    //     width: "20ch"
+    //   }
+    // },
     sectionDesktop: {
       display: "none",
       [theme.breakpoints.up("md")]: {
@@ -155,8 +155,8 @@ export default function Navgiation() {
           </div>
           <div class="rightNav">
             <div className={classes.search} id="searchBar">
-              <div className={classes.searchIcon}>
-                <SearchIcon />
+              <div className="searchIcon">
+                <SearchIcon/>
               </div>
               <InputBase
                 placeholder="Search by title..."
@@ -165,12 +165,12 @@ export default function Navgiation() {
                   input: classes.inputInput,
                 }}
                 inputProps={{ 'aria-label': 'search' }}
-                id = "searchInput"
+                className = "searchInput"
               />
             </div>
             <div className={classes.search} id="searchBar">
-              <div className={classes.searchIcon}>
-                <SearchIcon />
+              <div className="searchIcon">
+                <SearchIcon/>
               </div>
               <InputBase
                 placeholder="Search by location…"
@@ -179,7 +179,7 @@ export default function Navgiation() {
                   input: classes.inputInput,
                 }}
                 inputProps={{ 'aria-label': 'search' }}
-                id = "searchInput"
+                className = "searchInput"
               />
             </div>
           
