04e46859b2253ec23e1e4b4cc921800593b1f5b6
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-10-27 15:35:55 -0400

    Add files via upload

--- FrontEnd/quick-pick/src/component/quickPick/Navigation.jsx
+++ FrontEnd/quick-pick/src/component/quickPick/Navigation.jsx
@@ -9,91 +9,92 @@ import IconButton from "@material-ui/core/IconButton";
 import Menu from '@material-ui/core/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import { Navigation } from '@material-ui/icons';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
+import { createMuiTheme } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     grow: {
-      flexGrow: 1
+        flexGrow: 1
     },
     menuButton: {
-      marginRight: theme.spacing(2)
+        marginRight: theme.spacing(2)
     },
     title: {
-      display: "none",
-      [theme.breakpoints.up("sm")]: {
-        display: "block"
-      }
+        display: "none",
+        [theme.breakpoints.up("sm")]: {
+            display: "block"
+        }
     },
-    // search: {
-    //   position: "relative",
-    //   borderRadius: theme.shape.borderRadius,
-    //   backgroundColor: fade(theme.palette.common.white, 0.15),
-    //   "&:hover": {
-    //     backgroundColor: fade(theme.palette.common.white, 0.25)
-    //   },
-    //   marginRight: theme.spacing(2),
-    //   marginLeft: 0,
-    //   width: "100%",
-    //   [theme.breakpoints.up("sm")]: {
-    //     marginLeft: theme.spacing(3),
-    //     width: "auto"
-    //   }
-    // },
-    // searchIcon: {
-    //   padding: theme.spacing(0, 2),
-    //   height: "100%",
-    //   position: "absolute",
-    //   pointerEvents: "none",
-    //   display: "flex",
-    //   alignItems: "center",
-    //   justifyContent: "center"
-    // },
-    // inputRoot: {
-    //   color: "inherit"
-    // },
-    // inputInput: {
-    //   padding: theme.spacing(1, 1, 1, 0),
-    //   // vertical padding + font size from searchIcon
-    //   paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    //   transition: theme.transitions.create("width"),
-    //   width: "100%",
-    //   [theme.breakpoints.up("md")]: {
-    //     width: "20ch"
-    //   }
-    // },
+    search: {
+        position: "relative",
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        "&:hover": {
+            backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: "100%",
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: theme.spacing(3),
+            width: "auto"
+        }
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: "100%",
+        position: "absolute",
+        pointerEvents: "none",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    inputRoot: {
+        color: "inherit"
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: "20ch"
+        }
+    },
     sectionDesktop: {
-      display: "none",
-      [theme.breakpoints.up("md")]: {
-        display: "flex"
-      }
+        display: "none",
+        [theme.breakpoints.up("md")]: {
+            display: "flex"
+        }
     }
   }));
+const theme = createMuiTheme({
+    props: {
+    // Name of the component ⚛️
+        MuiButtonBase: {
+            // The properties to apply
+            disableRipple: true, // No more ripple, on the whole application 💣!
+        },
+    },
+});
 
 export default function Navgiation() {
     const classes = useStyles();
     const [anchorEl, setAnchorEl] = React.useState(null);
     const isMenuOpen = Boolean(anchorEl);
     const menuId = "primary-search-account-menu";
-    var dash = 'active';
-    var other = 'hidden';
-	const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
-	let user = sessionStorage.getItem('authenticatedUser');
+    const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+    let user = sessionStorage.getItem('authenticatedUser');
     const handleProfileMenuOpen = (event) => {
         setAnchorEl(event.currentTarget);
     };
-    const handleNavButtonClick = (event) => {
-        // accessible
-        if (event.target.class == "navButton dash" && event.target.id == "hidden"){
-            event.target.id = "active";
-        }
-     };
+
     const handleMenuClose = () => {
         setAnchorEl(null);
     };
     const handleMenuCloseLogout = () => {
-		setAnchorEl(null);
-		AuthenticationService.logout();
+        setAnchorEl(null);
+        AuthenticationService.logout();
     };
     var renderMenu = (
         // Add information here to check if already logged in and have a seperate menu with if conditionals
@@ -111,16 +112,86 @@ export default function Navgiation() {
             <Link to="/register" class="profileMenuLink"><MenuItem onClick={handleMenuClose} id="accountIconMenuItem">Register</MenuItem></Link>
         </Menu>
     );
-    function changeActive(path){
-        console.log(other);
-        if (path == "/"){
-            dash = 'active';
-            other = 'hidden';
-        } else if (path == "/other"){
-            dash = 'hidden';
-            other = 'active';
-        }
+
+    if (isUserLoggedIn == true){
+        renderMenu = (
+            // Add information here to check if already logged in and have a seperate menu with if conditionals
+            
+            <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+            >
+                <Link to={"/profile/"+user} class="profileMenuLink"><MenuItem onClick={handleMenuClose} id="accountIconMenuItem">Profile</MenuItem></Link>
+                <Link to="/" class="profileMenuLink"><MenuItem onClick={handleMenuCloseLogout} id="accountIconMenuItem">Log Out</MenuItem></Link>
+            </Menu>
+        );
+    }
+    var dash = 'active';
+    var other = 'hidden';
+    
+
+    //   id={this.index}
+    return (
+        <div className="navBar">
+            <div class="leftNav">
+                <img src={logo} alt="logo" class="logo"/>
+                <nav class="navControls">
+                    <Link class="navButton" id={dash} to={"/"}>Dashboard</Link>
+                    <Link class="navButton" id={other} to="/other">Other</Link>
+                    {/* <td><NavButton page={"hidden"} name={"Profile"} to={"/profile"}/></td> */}
+                </nav>
+            </div>
+            <div class="rightNav">
+                <MenuItem onClick={handleProfileMenuOpen} class="account">
+                    <IconButton >
+                        <AccountCircle id="accountIcon"/>
+                    </IconButton>
+                </MenuItem>
+                {renderMenu}
+            </div>
+        </div>
+    )
+}
+
+function MenuIcon() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const isMenuOpen = Boolean(anchorEl);
+    const menuId = "primary-search-account-menu";
+    const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+    let user = sessionStorage.getItem('authenticatedUser');
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
     };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+    const handleMenuCloseLogout = () => {
+        setAnchorEl(null);
+        AuthenticationService.logout();
+    };
+    var renderMenu = (
+        // Add information here to check if already logged in and have a seperate menu with if conditionals
+        <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMenuOpen}
+
+        onClose={handleMenuClose}
+        >
+            <Link to="/login" class="profileMenuLink"><MenuItem onClick={handleMenuClose} id="accountIconMenuItem">Login</MenuItem></Link>
+            <Link to="/register" class="profileMenuLink"><MenuItem onClick={handleMenuClose} id="accountIconMenuItem">Register</MenuItem></Link>
+        </Menu>
+    );
 
     if (isUserLoggedIn == true){
         renderMenu = (
@@ -141,58 +212,11 @@ export default function Navgiation() {
         );
     }
 
-    //   id={this.index}
     return (
-      <div class="navBar"> 
-          <img src={logo} alt="logo" class="logo"/>
-          <div class="navControls">
-              
-              <table>
-                  <td><Link class="navButton dash" onClick={handleNavButtonClick} id={dash} to={"/"}>Dash</Link></td>
-                  <td><Link class="navButton other" onClick={handleNavButtonClick} id={other} to="/other">Other</Link></td>
-                  {/* <td><NavButton page={"hidden"} name={"Profile"} to={"/profile"}/></td> */}
-              </table>
-          </div>
-          <div class="rightNav">
-            <div className={classes.search} id="searchBar">
-              <div className="searchIcon">
-                <SearchIcon/>
-              </div>
-              <InputBase
-                placeholder="Search by title..."
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-                className = "searchInput"
-              />
-            </div>
-            <div className={classes.search} id="searchBar">
-              <div className="searchIcon">
-                <SearchIcon/>
-              </div>
-              <InputBase
-                placeholder="Search by location…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-                className = "searchInput"
-              />
-            </div>
-          
-          <MenuItem onClick={handleProfileMenuOpen} class="account">
-              <IconButton >
-                  <AccountCircle id="accountIcon"/>
-              </IconButton>
-          </MenuItem>
-          </div>
-          {renderMenu}
-      </div>
-  )
-    
+        <div className="rightNav">
+            
+        </div>
+    )
 }
 
 Navigation.defaultProps = {
