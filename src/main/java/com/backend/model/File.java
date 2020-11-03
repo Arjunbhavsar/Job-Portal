4c9f6a090bc8fc00eff7ea9759ece7ec4a2f5ed2
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-03 00:00:03 -0500

    Update File.java

--- src/main/java/com/backend/model/File.java
+++ src/main/java/com/backend/model/File.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "files")
+@Table(name = "\"files\"")
 public class File {
 	@Id
 //	@GeneratedValue(strategy = GenerationType.IDENTITY)
