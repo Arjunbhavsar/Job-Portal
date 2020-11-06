86a570d9ab882279ccb00676734e4c84e8fef32e
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-05 23:38:59 -0500

    Add files via upload

--- src/main/java/com/backend/controller/FileController.java
+++ src/main/java/com/backend/controller/FileController.java
@@ -1,88 +1,110 @@
 package com.backend.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.backend.message.ResponseFile;
-import com.backend.message.ResponseMessage;
-import com.backend.model.File;
+import com.backend.dao.UserDao;
+import com.backend.message.UploadFileResponse;
 import com.backend.service.FileService;
-import com.backend.service.UserService;
 
-@Controller
-//@CrossOrigin("http://localhost:3000")	// local
-@CrossOrigin(origins = "*")	// heroku
+import com.backend.model.File;
+import com.backend.model.User;
+
+@RestController
+@CrossOrigin(origins = "*") // local
 public class FileController {
-
 	@Autowired
 	private FileService fileService;
-	
 	@Autowired
-	private UserService userService;
-	
-	@PostMapping("/upload/resume/{username}")
-	public ResponseEntity<ResponseMessage> uploadResume(@PathVariable String username, @RequestParam("file") MultipartFile file) {
+	private UserDao userDao;
+
+	@PostMapping("/uploadProfile/{username}")
+	public UploadFileResponse uploadProfile(@PathVariable String username, @RequestParam("file") MultipartFile file) {
 		String message = "";
 		try {
-			File tmpFile = fileService.storeResume(file, userService.getUser(username));
-			if(tmpFile == null)
-				message = "Invalid file type: " + file.getOriginalFilename();
-			else
-				message = "Uploaded the file successfully: " + file.getOriginalFilename();
-			return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+			File newFile = fileService.storeProfile(username, file);
+			User user = userDao.findByusername(username);
+			user.setProfileFileId(newFile.getId());
+			userDao.save(user);
+			String fileLoadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+	                .path("/load/")
+	                .path(newFile.getId())
+	                .toUriString();
+			return new UploadFileResponse(newFile.getName(), fileLoadUri, file.getContentType(), file.getSize());
+//			message = "Uploaded the file successfully: " + file.getOriginalFilename();
+//			return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
 		} catch (Exception e) {
 			message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-		}
-	}
-	
-	@PostMapping("/upload/profile/{username}")
-	public ResponseEntity<ResponseMessage> uploadProfile(@PathVariable String username, @RequestParam("file") MultipartFile file) {
-		String message = "";
-		try {
-			File tmpFile = fileService.storeProfile(file, userService.getUser(username));
-			if(tmpFile == null)
-				message = "Invalid file type: " + file.getOriginalFilename();
-			else
-				message = "Uploaded the file successfully: " + file.getOriginalFilename();
-			return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-		} catch (Exception e) {
-			message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+			return new UploadFileResponse(message);
+//			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
 		}
 	}
 
-	@GetMapping("/files")
-	public ResponseEntity<List<ResponseFile>> getListFiles() {
-		List<ResponseFile> files = fileService.getAllFiles().map(file -> {
-			String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/files/")
-					.path(file.getId()).toUriString();
-
-			return new ResponseFile(file.getName(), fileDownloadUri, file.getType(), file.getData().length);
-		}).collect(Collectors.toList());
-
-		return ResponseEntity.status(HttpStatus.OK).body(files);
+	@PostMapping("/uploadResume/{username}")
+	public UploadFileResponse uploadResume(@PathVariable String username, @RequestParam("file") MultipartFile file) {
+		String message = "";
+		try {
+			File newFile = fileService.storeResume(username, file);
+			User user = userDao.findByusername(username);
+			user.setProfileFileId(newFile.getId());
+			userDao.save(user);
+			String fileLoadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+	                .path("/load/")
+	                .path(newFile.getId())
+	                .toUriString();
+			return new UploadFileResponse(newFile.getName(), fileLoadUri, file.getContentType(), file.getSize());
+//			message = "Uploaded the file successfully: " + file.getOriginalFilename();
+//			return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+		} catch (Exception e) {
+			message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+			return new UploadFileResponse(message);
+//			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+		}
 	}
 
-	@GetMapping("/files/{id}")
-	public ResponseEntity<byte[]> getFile(@PathVariable String id) {
-		File file = fileService.getFile(id);
+	@GetMapping("/getProfile/{username}")
+	public ResponseEntity<byte[]> getProfile(@PathVariable String username) {
+		File file = fileService.getProfile(username);
 
 		return ResponseEntity.ok()
 				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
 				.body(file.getData());
+	}
+
+	@GetMapping("/getResume/{username}")
+	public ResponseEntity<byte[]> getResume(@PathVariable String username) {
+		File file = fileService.getResume(username);
+
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+				.body(file.getData());
+	}
+	
+	@GetMapping("/load/{id}")
+	public ResponseEntity<byte[]> getFile(@PathVariable String id) {
+		File file = fileService.getFile(id);
+
+		return ResponseEntity.ok()
+				.contentType(MediaType.parseMediaType(file.getType()))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
+				.body(file.getData());
+	}
+	
+	@GetMapping("/getFileName/{id}")
+	public String getFileName(@PathVariable String id) {
+		File file = fileService.getFile(id);
+		return file.getName();
 	}
 }
