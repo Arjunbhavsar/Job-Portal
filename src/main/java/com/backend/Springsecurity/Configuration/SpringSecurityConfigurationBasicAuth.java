8de6148c86395c9ba2b77ffd148219d233a21c13
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-09 21:47:47 -0500

    Update SpringSecurityConfigurationBasicAuth.java

--- src/main/java/com/backend/Springsecurity/Configuration/SpringSecurityConfigurationBasicAuth.java
+++ src/main/java/com/backend/Springsecurity/Configuration/SpringSecurityConfigurationBasicAuth.java
@@ -3,6 +3,7 @@ package com.backend.Springsecurity.Configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -20,7 +21,11 @@ public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerA
 			.antMatchers(HttpMethod.OPTIONS,"/**").permitAll()
 				.anyRequest().authenticated()
 				.and()
-//			.formLogin().and()
 			.httpBasic();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/load/**");
 	}
 }
