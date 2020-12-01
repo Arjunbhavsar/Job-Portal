8021c0ebb6562a83dc65d6b3b0ba0b171c18ca7b
markus-peterson <60580974+markus-peterson@users.noreply.github.com>
2020-11-30 19:09:52 -0500

    Making search queries not case sensitive

--- src/main/java/com/backend/dao/JobDao.java
+++ src/main/java/com/backend/dao/JobDao.java
@@ -20,17 +20,17 @@ public interface JobDao extends JpaRepository<Job, String>{
 	
 	List<Job> findAllByAuthor(String author);
 
-	@Query(value = "select * FROM \"job\" j WHERE j.job_title LIKE CONCAT('%',:searchKey,'%') AND j.location LIKE CONCAT('%',:location,'%')",
+	@Query(value = "select * FROM \"job\" j WHERE LOWER(j.job_title) LIKE LOWER(CONCAT('%',:searchKey,'%')) AND LOWER(j.location) LIKE LOWER(CONCAT('%',:location,'%'))",
 			nativeQuery = true)
 	List<Job> findBySearchParams(
 	  @Param("searchKey") String searchKey, 
 	  @Param("location") String location);
 	
-	@Query(value = "select * FROM \"job\" j WHERE j.location LIKE CONCAT('%',:location,'%')",
+	@Query(value = "select * FROM \"job\" j WHERE LOWER(j.location) LIKE LOWER(CONCAT('%',:location,'%'))",
 			nativeQuery = true)
 	List<Job> findBySearchLocation(@Param("location") String location);
 	
-	@Query(value = "select * FROM \"job\" j WHERE j.job_title LIKE CONCAT('%',:searchKey,'%')",
+	@Query(value = "select * FROM \"job\" j WHERE LOWER(j.job_title) LIKE LOWER(CONCAT('%',:searchKey,'%'))",
 			nativeQuery = true)
 	List<Job> findBySearchKeyword(@Param("searchKey") String searchKey);
 	
